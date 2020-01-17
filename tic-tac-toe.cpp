ed4515a6fdf0ac99f78d3de5877e93248f68bc10
2miles <miles4599@gmail.com>
2020-01-16 19:18:44 -0800

    added *.out to .gitignore and made a blank ticTacToe class with a constructer with a print message

---
+++ tic-tac-toe.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+#include "tic-tac-toe.h"
+
+using namespace std;
+
+
+ticTacToe::ticTacToe()
+{
+    cout << endl;
+    cout << "this is a blank Tic Tac Toe game object";
+    cout << endl;
+    cout << endl;
+}
+
+
+
+
+
