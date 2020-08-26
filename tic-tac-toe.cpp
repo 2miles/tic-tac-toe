2b21e6600bf291b5b9d2dd5034a678f181e88c5f
2miles <miles4599@gmail.com>
2020-08-26 14:23:05 -0700

    not much

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -188,10 +188,8 @@ bool ticTacToe::checkForTie(int turn)
 void ticTacToe::clearBoard()
 {
     for(int i = 0; i < 3; i++)
-    {
         for(int j = 0; j < 3; j++)
            board[i][j] = ' ';
-    }
 }
 
 //prints char board[][] as a board////////////////////////
